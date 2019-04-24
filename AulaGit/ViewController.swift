f6d3b6d8408dd172eedd3c00a75ca44bb9397337
Eduarda Linhares Mello <duda.mello1@hotmail.com>
2019-04-24 09:08:20 -0300

    Aprendendo a descartar

--- AulaGit/ViewController.swift
+++ AulaGit/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Abre a primeira tela do projeto")
+        
+        print("Isso está certo")
+        
     }
 
 
